26aebd32b4ea2fd8db410b0491d8d875c5ffd942
Zeggo <zeggolisko@gmail.com>
2023-03-04 01:34:36 +0200

    demon timing

--- EkaDemoni/tmp.bulk.compo.js
+++ EkaDemoni/tmp.bulk.compo.js
@@ -2032,103 +2032,13 @@ var CPlayer = function() {
       songData: [
         { // Instrument 0
           i: [
-          2, // OSC1_WAVEFORM
-          100, // OSC1_VOL
-          128, // OSC1_SEMI
-          0, // OSC1_XENV
-          3, // OSC2_WAVEFORM
-          201, // OSC2_VOL
-          128, // OSC2_SEMI
-          0, // OSC2_DETUNE
-          0, // OSC2_XENV
-          0, // NOISE_VOL
-          0, // ENV_ATTACK
-          24, // ENV_SUSTAIN
-          15, // ENV_RELEASE
-          0, // ENV_EXP_DECAY
-          0, // ARP_CHORD
-          0, // ARP_SPEED
-          0, // LFO_WAVEFORM
-          57, // LFO_AMT
-          6, // LFO_FREQ
-          1, // LFO_FX_FREQ
-          2, // FX_FILTER
-          193, // FX_FREQ
-          0, // FX_RESONANCE
-          2, // FX_DIST
-          35, // FX_DRIVE
-          147, // FX_PAN_AMT
-          6, // FX_PAN_FREQ
-          121, // FX_DELAY_AMT
-          6 // FX_DELAY_TIME
-          ],
-          // Patterns
-          p: [1,2,3,4,6,2,3,4,1,2,3,6,2,2],
-          // Columns
-          c: [
-            {n: [111,,123,,135,,111,,123,,135,,111,,123,,137,,111,,123,,137,,111,,123,,137,,139],
-             f: []},
-            {n: [111,111,123,,135,,111,,123,,135,,111,,123,,137,,111,,123,,137,,139,,111,,123,,125,,142],
-             f: []},
-            {n: [109,,121,,133,,109,,121,,133,,109,,121,,135,,109,,121,,135,,109,,121,,135,,137],
-             f: []},
-            {n: [104,104,116,,128,,116,,104,104,116,,128,,128,,104,104,116,,116,,104,,106,118,118,106,121,133,133,121],
-             f: []},
-            {n: [103,,115,,127,,103,,115,,127,,103,,115,,113,,101,,125,,113,,101,,125,,113,,118],
-             f: []},
-            {n: [111,111,123,,135,,111,,123,,135,,111,,123,,137,,111,,123,,137,,139,,123,,140,,152,,142,,,,,,,,,,,,,,,,144,,,,,,,,145,,,,147],
-             f: []}
-          ]
-        },
-        { // Instrument 1
-          i: [
-          2, // OSC1_WAVEFORM
-          100, // OSC1_VOL
-          128, // OSC1_SEMI
-          0, // OSC1_XENV
-          3, // OSC2_WAVEFORM
-          201, // OSC2_VOL
-          128, // OSC2_SEMI
-          0, // OSC2_DETUNE
-          0, // OSC2_XENV
-          0, // NOISE_VOL
-          0, // ENV_ATTACK
-          24, // ENV_SUSTAIN
-          17, // ENV_RELEASE
-          0, // ENV_EXP_DECAY
-          0, // ARP_CHORD
-          0, // ARP_SPEED
-          0, // LFO_WAVEFORM
-          57, // LFO_AMT
-          6, // LFO_FREQ
-          1, // LFO_FX_FREQ
-          2, // FX_FILTER
-          193, // FX_FREQ
-          0, // FX_RESONANCE
-          0, // FX_DIST
-          32, // FX_DRIVE
-          147, // FX_PAN_AMT
-          6, // FX_PAN_FREQ
-          121, // FX_DELAY_AMT
-          6 // FX_DELAY_TIME
-          ],
-          // Patterns
-          p: [,,1,,1,,1],
-          // Columns
-          c: [
-            {n: [156,154,,149],
-             f: []}
-          ]
-        },
-        { // Instrument 2
-          i: [
           0, // OSC1_WAVEFORM
           255, // OSC1_VOL
-          116, // OSC1_SEMI
+          117, // OSC1_SEMI
           64, // OSC1_XENV
           0, // OSC2_WAVEFORM
           255, // OSC2_VOL
-          116, // OSC2_SEMI
+          110, // OSC2_SEMI
           0, // OSC2_DETUNE
           64, // OSC2_XENV
           0, // NOISE_VOL
@@ -2145,66 +2055,106 @@ var CPlayer = function() {
           2, // FX_FILTER
           14, // FX_FREQ
           0, // FX_RESONANCE
-          0, // FX_DIST
-          32, // FX_DRIVE
-          0, // FX_PAN_AMT
-          0, // FX_PAN_FREQ
+          1, // FX_DIST
+          39, // FX_DRIVE
+          76, // FX_PAN_AMT
+          5, // FX_PAN_FREQ
           0, // FX_DELAY_AMT
           0 // FX_DELAY_TIME
           ],
           // Patterns
-          p: [,,,,1,1,1,1,1],
+          p: [1,1,1,1,1,1,1,1,1,1],
           // Columns
           c: [
-            {n: [135,,,135,135,,,135,135,,,135,135,,,135,135,,,135,135,,,135,135,,,135,135,,,135],
+            {n: [138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,138,,104,,107,109,,116,,116,,122,,118,,122,,118,,118,,120,,116,,116,,118,,116,,115,,115],
              f: []}
           ]
         },
-        { // Instrument 3
+        { // Instrument 1
           i: [
-          0, // OSC1_WAVEFORM
-          0, // OSC1_VOL
-          140, // OSC1_SEMI
+          1, // OSC1_WAVEFORM
+          100, // OSC1_VOL
+          128, // OSC1_SEMI
           0, // OSC1_XENV
-          0, // OSC2_WAVEFORM
-          0, // OSC2_VOL
-          140, // OSC2_SEMI
-          0, // OSC2_DETUNE
+          3, // OSC2_WAVEFORM
+          201, // OSC2_VOL
+          128, // OSC2_SEMI
+          2, // OSC2_DETUNE
           0, // OSC2_XENV
-          60, // NOISE_VOL
-          4, // ENV_ATTACK
-          10, // ENV_SUSTAIN
-          34, // ENV_RELEASE
+          0, // NOISE_VOL
+          0, // ENV_ATTACK
+          6, // ENV_SUSTAIN
+          49, // ENV_RELEASE
           0, // ENV_EXP_DECAY
           0, // ARP_CHORD
           0, // ARP_SPEED
           0, // LFO_WAVEFORM
-          187, // LFO_AMT
-          5, // LFO_FREQ
-          0, // LFO_FX_FREQ
-          1, // FX_FILTER
-          239, // FX_FREQ
-          135, // FX_RESONANCE
-          0, // FX_DIST
-          32, // FX_DRIVE
-          108, // FX_PAN_AMT
-          5, // FX_PAN_FREQ
-          16, // FX_DELAY_AMT
-          4 // FX_DELAY_TIME
+          139, // LFO_AMT
+          4, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          3, // FX_FILTER
+          30, // FX_FREQ
+          184, // FX_RESONANCE
+          119, // FX_DIST
+          244, // FX_DRIVE
+          147, // FX_PAN_AMT
+          6, // FX_PAN_FREQ
+          84, // FX_DELAY_AMT
+          6 // FX_DELAY_TIME
           ],
           // Patterns
-          p: [,,,,1,1,1,1,1,1,,,1,1,1],
+          p: [,,,1,,,1,,,1],
           // Columns
           c: [
-            {n: [,,135,,,,135,,,,135,,,,135,,,,135,,,,135,,,,135,,,135,135],
+            {n: [103,,,,,,,,103,,,,,,,,103,,,,,,,,103],
+             f: []}
+          ]
+        },
+        { // Instrument 2
+          i: [
+          2, // OSC1_WAVEFORM
+          255, // OSC1_VOL
+          140, // OSC1_SEMI
+          0, // OSC1_XENV
+          1, // OSC2_WAVEFORM
+          154, // OSC2_VOL
+          128, // OSC2_SEMI
+          9, // OSC2_DETUNE
+          0, // OSC2_XENV
+          55, // NOISE_VOL
+          0, // ENV_ATTACK
+          0, // ENV_SUSTAIN
+          52, // ENV_RELEASE
+          0, // ENV_EXP_DECAY
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          0, // LFO_AMT
+          0, // LFO_FREQ
+          0, // LFO_FX_FREQ
+          2, // FX_FILTER
+          255, // FX_FREQ
+          37, // FX_RESONANCE
+          15, // FX_DIST
+          32, // FX_DRIVE
+          47, // FX_PAN_AMT
+          3, // FX_PAN_FREQ
+          146, // FX_DELAY_AMT
+          2 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [,1,,,1,,,1,1,1],
+          // Columns
+          c: [
+            {n: [99,,,101,,101,,99,,105,,,,99,,107,,105,,,,107,,105,,99,,107,,,107],
              f: []}
           ]
         },
       ],
       rowLen: 5703,   // In sample lengths
       patternLen: 32,  // Rows per pattern
-      endPattern: 14,  // End pattern
-      numChannels: 4  // Number of channels
+      endPattern: 9,  // End pattern
+      numChannels: 3  // Number of channels
     };
 /* -*- mode: javascript; tab-width: 4; indent-tabs-mode: nil; -*- */
 
@@ -2428,6 +2378,10 @@ function buildSceneAtTime(t){
                               o:[new Material(pit)],
                               c:[horn(1)] //pit
                              },
+                             {f:[translate_wi(0,-0.2,0),scaleXYZ_wi(100,0.1,100)],
+                                o:[new Material(ctausta)],
+                                c:[horn(1)] //ground
+                               },
 /* 
                               {f:[translate_wi(0,-3,0),scaleXYZ_wi(2,2,2)],
                                o:[new Material(cpohja),objBall],
@@ -2444,7 +2398,7 @@ function buildSceneAtTime(t){
                                o:[],
                                c:[tausta]
                               },
-                              {f:[translate_wi(0,3,0), rotY_wi(t*.16), translate_wi(0,0,20-10*Math.sin(t*.01)), rotX_wi(.2)],
+                              {f:[translate_wi(0,4,0), rotY_wi(t*.16), translate_wi(0,0,20-10*Math.sin(t*.01)), rotX_wi(.2)],
                                 o:[],
                                 c:[],
                                 r:[new Camera()]
